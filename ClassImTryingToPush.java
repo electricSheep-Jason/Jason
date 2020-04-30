129d4ffda0376ef9d51b85a244ad208e194eaf1d
Yvonne Zino <y_zino@coloradocollege.edu>
2020-04-30 11:50:02 -0700

    trying to sign a commit

--- ClassImTryingToPush.java
+++ ClassImTryingToPush.java
@@ -1,5 +1,6 @@
 public class ClassImTryingToPush {
     public static void main(String [] args){
     System.out.println("Testing out pushing a new class from a shared repository");
+    System.out.println("Making a chang to try signing a commit");
     }
 }
